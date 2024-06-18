a41ef6fecb31345cf789792ba57edf8d6b21503e
BACHTUVU\BACHTUVU <bachtuvu.dev@gmail.com>
2024-06-19 02:06:50 +0700

    update code deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl:'http://10.2.6.142:8092/taskManagement/api',
+  apiUrl:'http://10.2.6.142:8092/bhq-ius/api',
   loginUrl: 'http://localhost:4200',
   gatewayUrl:'http://localhost:8055'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  // apiUrl:'http://172.17.80.1:8098/taskManagement/api',
-  apiUrl:'http://localhost:8055/taskManagement/api',
-  // apiUrl:'http://localhost:8098/taskManagement/api',
+  // apiUrl:'http://172.17.80.1:8098/bhq-ius/api',
+  apiUrl:'http://localhost:8055/bhq-ius/api',
+  // apiUrl:'http://localhost:8098/bhq-ius/api',
   gatewayUrl:'http://localhost:8055',
-  // apiUrl: 'http://host.docker.internal:8055/taskManagement/api',
+  // apiUrl: 'http://host.docker.internal:8055/bhq-ius/api',
   // gatewayUrl: 'http://host.docker.internal:8055',
 
-  // apiUrl:'http://10.2.6.142:8092/taskManagement/api'
+  // apiUrl:'http://10.2.6.142:8092/bhq-ius/api'
   // loginUrl: 'http://localhost:4200',
   // taskUrl: 'http://localhost:4201'
 };
